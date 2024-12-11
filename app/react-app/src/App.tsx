f6bc1c5d4c85fe158296e443c037ba8f27800d7b
nigaor <ktnkuhi0116@gmail.com>
2024-12-12 00:43:43 +0900

    12/12 commit

--- app/react-app/src/App.tsx
+++ app/react-app/src/App.tsx
@@ -1,15 +1,17 @@
 import React,{useState} from 'react';
 import './App.css';
 
-function Square() {
-  const [value,setValue] = useState<string | null >(null);
-  function handleClick(){
-    setValue("X");
-  }
+type Props = {
+  value:string|null;
+  onSquareClick:any;
+}
+
+function Square(props:Props) {
+  const {value,onSquareClick} = props;
   return(
   <button
     className="square"
-    onClick={handleClick}
+    onClick={onSquareClick}
   >
     {value}
   </button>
@@ -17,27 +19,73 @@ function Square() {
 }
 
 function Board() {
+  const [xIsNext,setXIsNext] = useState(true);
+  const [squares,setSquares] = useState<string[] | null[] >(Array(9).fill(null));
+  const winner = calculateWinner(squares);
+  let status;
+
+  if(winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X":"O");
+  }
+
+  function handleClick(i:number) {
+    if(squares[i] || calculateWinner(squares)){
+      return;
+    }
+    const nextSquares = squares.slice();
+    if(xIsNext){
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
+    setSquares(nextSquares);
+    setXIsNext(!xIsNext);
+  }
 
   return (
     <>
+      <div className="status">{status}</div>
       <div className="board-row">
-        <Square />
-        <Square /> 
-        <Square />
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)}/> 
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)}/>
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)}/>
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
       </div>
     </>
   );
 }
 
+function calculateWinner(squares:string[]|null[]):string|null {
+  const lines = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+  ];
+  for (let i = 0; i < lines.length; i++){
+    const[a,b,c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+//最後GPTにTypescriptに変換するように指示する
 
 export default Board;
